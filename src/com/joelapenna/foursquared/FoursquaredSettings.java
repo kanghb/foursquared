172e555d5d080e2847ba49a9afa8b9a1828fbb04
= <=>
2013-05-26 13:25:08 +0800

    first commit

---
+++ src/com/joelapenna/foursquared/FoursquaredSettings.java
@@ -0,0 +1,13 @@
+package com.joelapenna.foursquared;
+
+public class FoursquaredSettings {
+	public static final boolean USE_DEBUG_SERVER = false;
+
+    public static final boolean DEBUG = false;
+    public static final boolean LOCATION_DEBUG = false;
+
+    public static final boolean USE_DUMPCATCHER = false;
+    public static final boolean DUMPCATCHER_TEST = false;
+    public static final boolean SHOW_VENUE_MAP_BUTTON_MORE = false;//后加
+    public static final long  CATEGORY_ICON_EXPIRATION = 0L;//后加
+}
